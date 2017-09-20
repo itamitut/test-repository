19cac819a990651719f09a9e2c314ddc5f469fe6
SergA <itamitut@yandex.ru>
2017-09-20 10:02:05 +0300

    Первый рабочий тест
    Поиск в Google

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class FirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+    @Test
+    public void FirstTest(){
+        driver.navigate().to("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver",Keys.ENTER );
+        try {
+            Thread.sleep(5000);   //Полюбоваться на результаты поиска
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
