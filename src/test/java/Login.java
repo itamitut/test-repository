a1710a3dd9e5c00eb1f86bad123331307d368ccf
SergA <itamitut@yandex.ru>
2017-09-22 13:25:19 +0300

    Логирование в admin магазина уточек

---
+++ src/test/java/Login.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+
+public class Login {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+    @Test
+    public void FirstTest() {
+        driver.navigate().to("http://localhost/litecart/public_html/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).sendKeys(Keys.ENTER);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+    //        wait.until(titleIs("webdriver - Поиск в Google"));
+        }
+    }
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
